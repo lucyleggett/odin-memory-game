f3f44197697b20efc2eb654efe421efc1452c400
lucyleggett <lucy.leggett@hotmail.com>
2026-09-17 14:14:36 +0800

    Apply holographic effect to cards

--- odin-memory-game/src/App.jsx
+++ odin-memory-game/src/App.jsx
@@ -10,6 +10,38 @@ let scoreData = {
   previous: [0],
 };
 
+const cardWrappers = document.querySelectorAll(".card-wrapper");
+
+cardWrappers.forEach((wrapper) =>
+  wrapper.addEventListener("mousemove", (e) => {
+    const card = wrapper.querySelector(".card");
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const xc = x - rect.width / 2;
+    const yc = y - rect.height / 2;
+
+    const px = (x / rect.width) * 100;
+    const py = (y / rect.height) * 100;
+
+    card.style.setProperty("--x", `${px}%`);
+    card.style.setProperty("--y", `${py}%`);
+    card.style.setProperty("--rx", `${-yc / 10}deg`);
+    card.style.setProperty("--ry", `${xc / 10}deg`);
+    card.style.setProperty("--o", "1");
+  }),
+);
+
+cardWrappers.forEach((wrapper) =>
+  wrapper.addEventListener("mouseleave", () => {
+    const card = wrapper.querySelector(".card");
+    card.style.setProperty("--rx", "0deg");
+    card.style.setProperty("--ry", "0deg");
+    card.style.setProperty("--o", "0");
+  }),
+);
+
 function App() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -47,8 +79,6 @@ function App() {
 
   if (loading) return <div className="message loading">Loading...</div>;
   if (error) return <div className="message error">Error: {error}</div>;
-
-  console.log(characters.map(character => retrieveName(character.image)));
 
   return (
     <>
--- odin-memory-game/src/components/Card.jsx
+++ odin-memory-game/src/components/Card.jsx
@@ -2,17 +2,20 @@ import helloKittyBow from "../assets/hello-kitty-red-bow-800x800.png"
 
 export default function Card({ imgSrc, imgTitle }) {
   return (
-    <div className="card-container">
+    <div className="card-wrapper">
       <div className="card">
-          <button className="card-face">
-            <img src={imgSrc} alt="" />
-            <span className="image-title">{imgTitle}</span>
-          </button>
-          <div className="card-back hidden">
-            <img src={helloKittyBow} alt="Hello Kitty's red bow" />
+          <div className="card-content">
+              <button className="card-face">
+                <img src={imgSrc} alt="" />
+                <span className="image-title">{imgTitle}</span>
+              </button>
+              <div className="card-back hidden">
+                <img src={helloKittyBow} alt="Hello Kitty's red bow" />
+              </div>
           </div>
+          <div className="glare-wrapper"></div>
+          <div className="holo-wrapper"></div>
       </div>
-      <div className="glare-wrapper"></div>
     </div>
   );
 }
