3d74c91a4c53857ffbd5a08b582d9cb4c8bf257d
lucyleggett <lucy.leggett@hotmail.com>
2026-09-18 17:29:16 +0800

    Add winning and losing screens

---
+++ odin-memory-game/src/components/Screen.jsx
@@ -0,0 +1,32 @@
+export default function Screen({
+  className,
+  gif,
+  gifAlt,
+  message,
+  handleNextGame,
+}) {
+  return (
+    <>
+      {handleNextGame && (
+        <div className={`${className} screen hidden`}>
+          <div className="blur-overlay">
+            <div className="conveyor">
+              <img src={gif} alt={gifAlt} />
+              <div className="message">{message}</div>
+              <button onClick={handleNextGame}>Play again</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {!handleNextGame && (
+        <div className={`${className} screen`}>
+          <div className="conveyor">
+            <img src={gif} alt={gifAlt} />
+            <div className="message">{message}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
